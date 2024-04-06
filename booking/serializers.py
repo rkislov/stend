6eb6e446f6e1df5afe541997c671931af685820f
Roman Kislov <roman@kislovs.ru>
2024-04-06 21:23:03 +0300

    overlaping

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Room, Resource, Booking
 
 
@@ -17,6 +18,22 @@ class ResourceSerializer(serializers.ModelSerializer):
 class BookingSerializer(serializers.ModelSerializer):
     room = RoomSerializer()
     resources = ResourceSerializer(many=True)
+
+    def validate(self, data):
+        room_id = data.get('room',{}).get('id')
+        resource_ids = [resource['id'] for resource in data.get('resources',[])]
+        start_time = data.get('strat_time')
+        end_time = data.get('end_time')
+
+        overlapping_bookings = Booking.objects.filter(
+            models.Q(room__id=room_id) | models.Q(resource__id__in=resource_ids),
+            start_time__lte=end_time, end_time__gte=start_time
+        ).exclude(id=self.instance.pk if self.instance else None)
+
+        if overlapping_bookings.exists():
+            raise exceptions.ValidationError('Уже забронировано')
+
+        return data
 
     class Meta:
         model = Booking
--- stend/settings.py
+++ stend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'booking'
+    'booking',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
